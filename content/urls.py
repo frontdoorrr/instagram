240e54c8332209a32140131083f61dceb64cde3b
leejeongmunn <lot9616@gmail.com>
2022-05-26 17:03:33 +0900

    UploadFeed 기능 수정 / url 매핑

---
+++ content/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import UploadFeed
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('upload/', UploadFeed.as_view()),
+    ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
