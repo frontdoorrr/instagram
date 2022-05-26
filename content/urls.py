a1d647ae9bf1f787c7dd46f0df6cf59f4cf79d47
leejeongmunn <lot9616@gmail.com>
2022-05-26 18:08:44 +0900

    피드 게시물 추가 기능 구현 중

--- content/urls.py
+++ content/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('upload/', UploadFeed.as_view()),
+    path('upload', UploadFeed.as_view()),
     ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
