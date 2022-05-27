d1351459d1e76b334a3f7d7a012599e3e9d52568
leejeongmunn <lot9616@gmail.com>
2022-05-27 10:26:29 +0900

    user model 생성

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'instagram',
     'content',
+    'user',
    
 ]
 
@@ -129,3 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+AUTH_USER_MODEL = 'user.User'
